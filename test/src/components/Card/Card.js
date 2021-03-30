7f5f8c91212c9e71403468c6fa31ccebbf14abda
Fojan Babaali <fozhanmail@gmail.com>
2021-03-29 21:47:31 -0700

    Create Card.js

---
+++ test/src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./Card.css"
+class Card extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div className="Card">
+                <h3>Role Management</h3>
+                <div>dsfsdgfsdgg</div>
+            </div>
+        );
+    }
+}
+ 
+export default Card;
