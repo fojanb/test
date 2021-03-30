867ea7e8d440d4ee972c11ed3e26c3ce1b29d006
Fojan Babaali <fozhanmail@gmail.com>
2021-03-29 21:44:29 -0700

    Update App.js

--- test/src/containers/App.js
+++ test/src/containers/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import SideBar from "../components/SideBar/SideBar"
-import './App.css';
+import React, { Component } from "react";
+import SideBar from "../components/SideBar/SideBar";
+import "./App.css";
 class App extends Component {
-  state = {  }
-  render() { 
-    return (  
-      <div>
+  state = {};
+  render() {
+    return (
+      <div className="App">
         <SideBar/>
       </div>
     );
   }
 }
- 
-export default App;
 
+export default App;
