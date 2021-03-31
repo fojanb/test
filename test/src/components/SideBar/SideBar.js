6ffd1a2706373502c130af6cfc1807849af9fcfd
Fojan Babaali <fozhanmail@gmail.com>
2021-03-30 18:23:47 -0700

    Update SideBar.js

--- test/src/components/SideBar/SideBar.js
+++ test/src/components/SideBar/SideBar.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import "./SideBar.css";
 class SideBar extends Component {
-  state = {};
+  state = {title : ''};
   render() {
+    // const setNewitem = () =>{
+    //   let box = document.createElement('div');
+    //   box.classList.add("items");
+    //   box.innerText = this.props.newItem;
+    //   let board = document.getElementById("SideBarStyles")
+    //   board.appendChild(box);
+
+    // }
     return (
-      <div className="SideBarStyles">
+      <div className="SideBarStyles" id="SideBarStyles">
         <h3>Side Bar Component</h3>
         <div className="items">Stats</div>
         <div className="items">Settings</div>
         <div className="items">Payment</div>
+        <div className="items">{this.props.newItem}</div>
         <div id="newCardName"></div>
       </div>
     );
