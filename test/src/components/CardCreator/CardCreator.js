5dcca2bf761a30e48209f4640400149427822c28
Fojan Babaali <fozhanmail@gmail.com>
2021-03-30 22:09:05 -0700

    Update CardCreator.js

--- test/src/components/CardCreator/CardCreator.js
+++ test/src/components/CardCreator/CardCreator.js
@@ -2,41 +2,38 @@ import React, { Component } from "react";
 import Card from "../Card/Card";
 import "./CardCreator.css";
 class CardCreator extends Component {
-  state = { showCard: false, title: "" };
+  state = {
+    titleArray: [ ],
+  };
 
   //---------------------Event Handlers Start------------------------
-  AddCardHandler = () => {
-    let doesShow = this.state.showCard;
-    this.setState({ showCard: !doesShow });
-  };
-  sendData = () => {
-    this.props.parentCallback(this.state.title);
+  createCard = () => {
+    const title = this.state.titleArray;
+    title.push( title );
+    this.setState({ titleArray: title });
   };
   //---------------------CallBack Handler/state management----------------------------
   callbackFunction = (childData) => {
-    this.setState({ title: childData });
+    const dataRecived = [];
+    dataRecived.push(childData);
+    this.setState({ titleArray: dataRecived });
   };
   //-----------------------------------------------------------------
   render() {
-    let card = null;
-    
-   
-    if (this.state.showCard) {
-      card = (
-        <div>
-          <Card parentCallback={this.callbackFunction} />
-
-        </div>
-      );
-    }
-
     return (
       <div>
         <div className="CardCreator">
-          <button onClick={this.AddCardHandler}>Create Card</button>
+          <button onClick={this.createCard}>Create Card</button>
+          <div>
+            {this.state.titleArray.map((title,index) => {
+              return (
+                <div className="card">
+                  <Card parentCallback={this.callbackFunction} key={index} />
+                </div>
+              );
+            })}
+          </div>
         </div>
-        {card}
-       <button onClick={this.sendData}>Add to sidebar</button>
       </div>
     );
   }
